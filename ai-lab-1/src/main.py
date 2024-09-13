ed3eae0cb288b9b97d9dff639a9fd401df1af383
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-09-13 10:57:23 +0300

    fix: rename to English

--- ai-lab-1/src/main.py
+++ ai-lab-1/src/main.py
@@ -6,32 +6,36 @@ from sklearn import preprocessing
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 
 
+TITLE = 'Lab 1'
 SOURCE_FILE = '../08_Сегментация покупателей/customers.xls'
 OBJECTS_COUNT = 184
 COLUMN_NAMES = ('пол код', 'возраст код', 'Телеканал', 'Профессия.1', 'Пресса')
+OBJECTS_DISTANCE_TYPE = 'ecuclidean'
+CLUSTERS_DISTANCE_TYPE = 'ward'
 
 
 def main():
+  # data import
   df = pd.read_excel(SOURCE_FILE, nrows=OBJECTS_COUNT, usecols=COLUMN_NAMES)
 
-  print('Imported data')
-  print(df.head())
-
+  # hierarcical clustering
   scaler = preprocessing.MinMaxScaler().fit(df.to_numpy())
   scaled_data = scaler.transform(df.to_numpy())
-  print('Transformed data')
-  print(pd.DataFrame(scaled_data))
+  distance_matrix = linkage(scaled_data, method=CLUSTERS_DISTANCE_TYPE, metric=OBJECTS_DISTANCE_TYPE)
 
-  distance_matrix = linkage(scaled_data, method='ward', metric='euclidean')
-
+  # dendrogram plotting
   fig = plt.figure(figsize=(15, 30))
   fig.patch.set_facecolor('white')
-  R = dendrogram(distance_matrix,
-                 orientation='top',
-                 leaf_font_size=12,)
+  dendrogram(distance_matrix,
+             orientation='top',
+             leaf_font_size=12,)
+
+  # k-means clustering
+  
+
   plt.show()
 
 
 if __name__ == '__main__':
-  tprint('Lab 1')
+  tprint(TITLE)
   main()
