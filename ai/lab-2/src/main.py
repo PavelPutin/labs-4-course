8e4c98d44d33a1a94ae5c85ac022cac51770196e
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-09-19 14:05:24 +0300

    feat[ai]: make forecast

--- ai/lab-2/src/main.py
+++ ai/lab-2/src/main.py
@@ -2,6 +2,9 @@ import pandas as pd
 from art import tprint
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 import locale
 
 
@@ -9,21 +12,51 @@ TITLE = 'Lab 2'
 SOURCE_FILE = '..\\11_Вина Автралии fort\\wine_Austral.dat'
 COLUMN_SEPARATOR = '\t'
 COLUMNS = ('fort', 'date_')
+FORECAST_PERIODS = 8
 
 
 def main():
   locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
   df = read_data()
-  plot_data(df)
+  df_trend = df[COLUMNS[0]].rolling(window=12).mean()
+  df_season = df[COLUMNS[0]].rolling(window=2).mean()
+
+  df['stationare'] = df[COLUMNS[0]] - df[COLUMNS[0]].rolling(window=2).mean()
+  df['stationare'] = df['stationare'].diff()
+  df.fillna(0, inplace=True)
+
+  forecast_column = COLUMNS[0]
+  model = ARIMA(df[forecast_column], order=(1, 1, 1))
+  model_fit = model.fit()
+  forecast = model_fit.forecast(steps=FORECAST_PERIODS)
+  mse = mean_squared_error(df[forecast_column][-FORECAST_PERIODS:], forecast)
+  mae = mean_absolute_error(df[forecast_column][-FORECAST_PERIODS:], forecast)
+  print(f'MSE: {mse}')
+  print(f'MAE: {mae}')
+
+  future_dates = pd.date_range(start=df[COLUMNS[1]][len(df[COLUMNS[1]]) - 1], periods=FORECAST_PERIODS, freq='M')
+  forecast_df = pd.DataFrame({
+    'date_': future_dates,
+    'forecast': forecast
+  })
+
+  df = df._append(forecast_df, ignore_index=True)
+
+  plot_data(df, df_trend, df_season)
 
 
-def plot_data(df):
+def plot_data(df, trend, season):
   fig, ax = plt.subplots()
-  ax.plot(df.get(COLUMNS[0]))
+  ax.plot(df[COLUMNS[0]][:-FORECAST_PERIODS], marker='o', linestyle='-', label='Продажи')
+  ax.plot(df['forecast'][-FORECAST_PERIODS:], marker='o', label='Прогноз')
+  ax.plot(trend, label='Тренд')
+  ax.plot(season, label='Сезонность', linestyle='--')
   ax.xaxis.set_major_locator(mticker.MultipleLocator(12))
   ax.xaxis.set_major_formatter(mticker.FuncFormatter(get_date_ticks(df.get(COLUMNS[1]))))
   ax.xaxis.set_minor_locator(mticker.MultipleLocator(1))
   ax.xaxis.set_minor_formatter(mticker.NullFormatter())
+  ax.grid(True)
+  ax.legend()
   plt.show()
 
 
@@ -35,7 +68,7 @@ def read_data():
 
 def get_date_ticks(dates):
   def inner(x, pos):
-    if 0 <= int(x) < len(dates) and int(x) % 12 == 0:
+    if 0 <= int(x) < len(dates):
       return dates[int(x)].strftime('%b %Y')
     return ''
   return inner
