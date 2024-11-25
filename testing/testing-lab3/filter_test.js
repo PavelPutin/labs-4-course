65d17c59e38179984a7b01c3719cfc7a620adbf1
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-11-25 08:39:11 +0300

    chore[testing]: remove dummy test

--- testing/testing-lab3/filter_test.js
+++
@@ -1,9 +0,0 @@
-Feature('filter');
-
-// Scenario('test something',  ({ I }) => {
-//   I.amOnPage('/');
-//   I.waitForNavigation();
-//   I.seeElementInDOM('button.ga-header__tab_type_search');
-//   I.click('button.ga-header__tab_type_search');
-//   I.seeElementInDOM({css: 'input[placeholder=\'хочу купить\']'})
-// });
