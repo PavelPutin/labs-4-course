e56ddb646f28430946470cde5f510fc3e7b76b69
Pavel Putin <pavelputin2003@yandex.ru>
2024-11-26 20:05:07 +0300

    feat[testing]: make test case 7

--- testing/testing-lab3/searching_test.js
+++ testing/testing-lab3/searching_test.js
@@ -1,40 +1,47 @@
 Feature('Поиск товаров (артикул, название)');
 
-Scenario('Поиск товара по названию (товар существует)', async ({ I, searchingPage }) => {
-  const searchQuery = 'anastasia beverly hills lash brag';
-  searchingPage.searchOnSite(searchQuery, () => {
-    I.see('ANASTASIA BEVERLY HILLS', searchingPage.locators.resultLabelCSSClass);
-  });
-});
+// Scenario('Поиск товара по названию (товар существует)', async ({ I, searchingPage }) => {
+//   const searchQuery = 'anastasia beverly hills lash brag';
+//   searchingPage.searchOnSite(searchQuery, () => {
+//     I.see('ANASTASIA BEVERLY HILLS', searchingPage.locators.resultLabelCSSClass);
+//   });
+// });
 
-Scenario('Поиск товара по артикулу (товар существует)', async ({ I, searchingPage }) => {
-  const searchQuery = '19000164782';
-  searchingPage.searchOnSite(searchQuery, () => {
-    I.see('ANASTASIA BEVERLY HILLS', searchingPage.locators.resultLabelCSSClass);
-    I.see('1 продукт');
-  });
-});
+// Scenario('Поиск товара по артикулу (товар существует)', async ({ I, searchingPage }) => {
+//   const searchQuery = '19000164782';
+//   searchingPage.searchOnSite(searchQuery, () => {
+//     I.see('ANASTASIA BEVERLY HILLS', searchingPage.locators.resultLabelCSSClass);
+//     I.see('1 продукт');
+//   });
+// });
 
-Scenario('Поиск товара по названию (товар не существует)', async ({ I, searchingPage }) => {
-  const searchQuery = 'абракадабра';
-  searchingPage.searchMissingOnSite(searchQuery);
-});
+// Scenario('Поиск товара по названию (товар не существует)', async ({ I, searchingPage }) => {
+//   const searchQuery = 'абракадабра';
+//   searchingPage.searchMissingOnSite(searchQuery);
+// });
 
-Scenario('Поиск товара по артикулу (товар не существует)', async ({ I, searchingPage }) => {
-  const searchQuery = '39000164782';
-  searchingPage.searchMissingOnSite(searchQuery);
-});
+// Scenario('Поиск товара по артикулу (товар не существует)', async ({ I, searchingPage }) => {
+//   const searchQuery = '39000164782';
+//   searchingPage.searchMissingOnSite(searchQuery);
+// });
 
-Scenario('Недоступность кнопки отправки поискового запроса', async ({ I, searchingPage }) => {
-  I.amOnPage('/');
-  I.waitForNavigation();
-  I.click(searchingPage.locators.openButton);
-  I.seeElementInDOM(searchingPage.locators.submitButtonDisabled);
-});
+// Scenario('Недоступность кнопки отправки поискового запроса', async ({ I, searchingPage }) => {
+//   I.amOnPage('/');
+//   I.waitForNavigation();
+//   I.click(searchingPage.locators.openButton);
+//   I.seeElementInDOM(searchingPage.locators.submitButtonDisabled);
+// });
 
 Scenario('Поиск по названию бренда', async ({ I, searchingPage }) => {
   const searchQuery = 'anastasia beverly hills';
   searchingPage.searchOnSite(searchQuery, () => {
     I.see('anastasia beverly hills', searchingPage.locators.resultLabelCSSClass);
+  });
+});
+
+Scenario('Исправление поисковых запросов с ошибками или опечатками', async ({ I, searchingPage }) => {
+  const searchQuery = 'makeап';
+  searchingPage.searchOnSite(searchQuery, () => {
+    I.see('запрос «makeап» был исправлен');
   });
 });
